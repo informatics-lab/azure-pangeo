b2f697bbf244e5c65bf152d5b0d0bd3472e1be17
Theo McCaie <tam203@gmail.com>
2019-09-06 21:41:39 +0100

    trying to fix travis

--- scripts/test-chart.sh
+++ scripts/test-chart.sh
@@ -5,5 +5,5 @@ echo "*** Test ***"
 for dir in env/*; do
     env=${dir##*/}
     echo "*** Test ${env} ***"
-    helm lint jadepangeo -f env/${env}/values.yaml -f env/${env}/secrets.yaml
+    helm lint azure-pangeo -f env/${env}/values.yaml -f env/${env}/secrets.yaml
 done
